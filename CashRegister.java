0d039515fe8c93cef651ce107465fa2f145a88de
Afrida Tabassum <afrida1009@gmail.com>
2019-10-19 21:42:14 +1100

    Add files via upload
    3 input text files. prices.txt holds product name and prices, other 2 holds customer bought products for 2 different customers.

---
+++ CashRegister.java
@@ -0,0 +1,202 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+/*
+ * CSIT111 Assignment 3 
+ * Student name:   Brody Brown
+ * Student user ID:  bb879 
+ * Student number:  6116310
+ */  
+
+import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
+import static java.lang.Math.pow;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.text.DecimalFormat;
+
+
+public class CashRegister{
+
+	private int totalItems;
+	private double totalSale ;
+	private double totalsubtotal;
+	private double totalofcart;
+	private int size;
+	private int customerCount;
+    private HashMap<String, Integer> productsSold=new HashMap<String, Integer>();
+	
+	List<Integer> productIDs = new ArrayList<Integer>();  //this will give quantity
+	List<Double> productprices = new ArrayList<Double>();  // this will give subtotal
+	
+	List<Product> products= new ArrayList<Product>();
+	List<Product> cartproducts= new ArrayList<Product>();
+	List<String> names= new ArrayList<String>();
+	
+			DecimalFormat df2= new DecimalFormat("#.##");
+
+	
+	 CashRegister(List <Product> p)
+	 {
+		 this.products=p;
+		 this.size=p.size();
+		 
+		 for(int i=0; i<size+1; i++)
+		 {
+			 this.productIDs.add(i,0);
+			 //System.out.println("size "+size);
+			 this.productprices.add(i,0.0);
+			 this.names.add(i,"");
+		 }
+	 }
+
+	public void scanProduct(Product item){
+		
+		this.totalsubtotal  += item.getPrice();
+		//System.out.println("now totalsale:"+ totalSale);
+		this.totalItems++;
+		
+		int id=item.getProductID();
+		//System.out.println(item.getName()+ " is: "+ id );
+		int sum=productIDs.get(id);  //0
+		//System.out.println(sum);   //0.0
+		double oldprice=productprices.get(id);
+		//System.out.println(oldprice);
+		double newprice=item.getPrice();
+		//System.out.println(newprice);
+		
+		this.productIDs.set(id,sum+1);  //1, 1; 2,1 . 2,2, 
+		//System.out.println("id: "+ id + ", sum= " +(sum+1)); 
+		this.productprices.set(id,oldprice+newprice);
+		//System.out.println("product quantity: "+ productIDs.get(id) + ", newprice added "+ (oldprice+newprice));
+		this.names.add(id,item.getName());
+		
+		//this.productID[item.getproductID()]++;
+		//this.productprices[item.getproductID()]=item.getprice();
+		
+	}
+	
+	public double getPurchase(){
+		double a=this.totalsubtotal;
+		this.totalSale+=this.totalsubtotal;
+		this.totalsubtotal=0;
+		return a;
+	}
+	
+	public double gettotalsale()
+	{
+		return this.totalSale;
+	}
+	
+	public int getTotal(){
+		
+		return this.totalItems;
+	}
+	
+	public void scanAllItems(List<Product> itemlist)
+	{
+		this.cartproducts=itemlist;
+		
+		for(Product t : itemlist)
+		{		
+			scanProduct(t);
+		}  
+	}
+	
+	
+	
+	public void printreceipt(ShoppingCart cart)
+	{
+		//System.out.println( "One customer is checking out ...");
+	    System.out.println( "=====================================");
+        System.out.println("Product     Price   Qty     Subtotal");
+        System.out.println( "-------------------------------------");
+		
+		 List<Product> purchaseItems = cart.getItems();
+         List<Product> cartproducts = new ArrayList<>(); 
+        Set<Product> set = new HashSet<>(purchaseItems);  			// Finding Unique Items In Cart
+        cartproducts.addAll(set);
+		
+		for(Product item : cartproducts)
+		{
+			//System.out.println(p.getProductID()); 				// gives all the product ids
+			
+			String pname=item.getName();
+			double unitprice=item.getPrice();
+			int qty=productIDs.get(item.getProductID());
+			double subtotal= productprices.get(item.getProductID());
+			
+			System.out.println(pname + "         " +"$" + unitprice+"       " +qty + "       " + "$"+ subtotal); // gives quantity
+			
+		
+			//if (productsSold.containsKey(item)){
+			productsSold.put(item.getName(), qty);
+			//productsSold.put("ghd", 2);
+
+           // } else {
+             //   productsSold.put(item.getName(), qty);
+           // }
+	
+		}
+		
+		//System.out.print("total of subtotal " + getPurchase());
+		
+       // System.out.println( "Bed" + "         " + "$" + "       " + "2" + "       " + "$"     );
+       
+        System.out.println( "   -------------------------");
+        System.out.println("         Total             "+ df2.format(getPurchase()));
+        System.out.println( "=====================================");
+		customerCount++;
+	}
+	
+	public void sortbykey()
+	{
+		ArrayList<String> sortedKeys = new ArrayList<String>(productsSold.keySet()); 
+		
+		Collections.sort(sortedKeys);
+		
+	}
+	
+	public void printreport()
+	{
+		
+        System.out.println("Report for the day");
+        System.out.println( "=====================================");
+        System.out.println("Number of Customers: " + customerCount);
+        //print("Total Sale: $" + df.format(totalSale));
+        System.out.println("List of products sold:");
+        System.out.println( "-------------------------");
+        System.out.println("Product Qty");
+        System.out.println( "-------------------------");
+		
+		ArrayList<String> sortedKeys = new ArrayList<String>(productsSold.keySet()); 
+		
+		Collections.sort(sortedKeys);
+		
+        for (String key : sortedKeys) 
+		{
+            System.out.println(key + " " + productsSold.get(key));
+        }
+
+	}
+	
+	
+	
+}   
+   
---
+++ POSmain.java
@@ -0,0 +1,245 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+/*
+ * CSIT111 Assignment 3 
+ * Student name:   Brody Brown
+ * Student user ID:  bb879 
+ * Student number:  6116310
+ */  
+
+import java.util.Scanner;
+import java.io.*;
+import static java.lang.Math.pow;
+import java.lang.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.text.DecimalFormat;
+import java.util.Collections;
+
+
+public class POSmain
+{
+
+	public static void main(String[] args) throws IOException {
+
+		FileInputStream file1=new FileInputStream(args[0]); //prices
+		FileInputStream file2=new FileInputStream(args[1]); //cart1
+		FileInputStream file3=new FileInputStream(args[2]);  //cart2
+
+		Scanner sc1 = new Scanner(file1);
+		Scanner sc2 = new Scanner(file2);
+		Scanner sc3 = new Scanner(file3);
+	  
+		List<Product> prodlist = new ArrayList<Product>();
+				
+		DecimalFormat df2= new DecimalFormat("#.##");
+		
+		/* DataInputStream in = new DataInputStream(file1);
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		String strLine;
+		 */
+
+		
+		while (sc1.hasNextLine())   {
+
+			 String line = sc1.nextLine();
+			 String[] tokens = line.split("\\s+");
+				
+			 String fitem = tokens[0];
+			 Double fprice = Double.parseDouble(tokens[1]);
+		   
+			Product obj = new Product(fitem,fprice);
+			prodlist.add(obj);
+		
+		
+			/* for(Product p : prodlist)
+			{
+					
+				System.out.println( "Arraylist : "+ p.displaylist());
+
+			}  */ 
+			
+            
+		}
+		
+		//Collections.sort(prodlist);
+			
+       sc1.close();
+	   
+	   // Create a ShoppingCart object
+		ShoppingCart cart1 = new ShoppingCart();
+	   
+	   while (sc2.hasNextLine())   {
+
+			 String line = sc2.nextLine();
+			 String[] token = line.split("\\s+");
+			 String word = token[0];
+			 
+			 for(int i=0; i<prodlist.size(); i++)
+			{
+				Product prod=prodlist.get(i);
+	
+				if( word.equals(prod.getName()))
+				{
+					cart1.addItem(prodlist.get(i));
+					break;
+
+				}
+			}
+			 
+			/* for(int i=0; i<prodlist.size(); i++)
+			{
+				
+				//System.out.println(item.getName());
+				System.out.println(prodlist.contains(line));
+				Product prod=prodlist.get(i);
+				
+				if(line.equals(prod.getName()))
+				{
+					int ind = i;
+					System.out.println(i);
+					/* System.out.println("index"+prodlist.indexOf(item));
+					int ind=prodlist.indexOf(item); 
+					break;
+
+				}
+			} 
+			*/
+			 
+			 //int i=getIndexbyname(line, prodlist);
+			 //System.out.println("indexsec "+i);
+			 //cart1.addItem(prodlist.get(i));
+	  
+			// Add items to the shopping cart
+			//for(int i=0; i< prodlist.size(); i++){
+				//System.out.println( prodlist.get(i).displaylist());
+				
+				//int ind=prodlist.indexOf(line);
+
+				//cart1.addItem(line);
+				
+		//	}
+			
+
+			/* Product obj = new Product(fitem,fprice);
+			prodlist.add(obj);
+		
+			for(Product p : prodlist)
+			{
+					
+					System.out.println( p.displaylist());
+
+			} */
+            
+		}
+		
+		
+		System.out.println(cart1.getTotalcarts() + " customer(s) in checking out with " + cart1.getTotalitems() + " items");	
+		sc2.close();
+	   
+	   
+	   // Create a ShoppingCart object
+		ShoppingCart cart2 = new ShoppingCart();
+	   
+	   while (sc3.hasNextLine())   {
+
+			 String line = sc3.nextLine();
+			 String[] token = line.split("\\s+");
+			 String word = token[0];
+			
+			 for(int i=0; i<prodlist.size(); i++)
+			{
+				Product prod=prodlist.get(i);
+	
+				if( word.equals(prod.getName()))
+				{
+					cart2.addItem(prodlist.get(i));
+					break;
+
+				}
+			}
+			 
+			 
+			 
+	
+            
+		}
+		
+		sc3.close();
+		
+		//System.out.println("Scan all items in the shopping cart");
+		
+		// ~~~~~~~~~~~~~~~~ CART1 ~~~~~~~~~~~~~~~~~~~
+
+		// Create a CashRegister object
+		CashRegister register = new CashRegister(prodlist);
+		List<Product> itemlist=cart1.unloadAllItems();
+		register.scanAllItems(itemlist);
+		register.printreceipt(cart1);
+
+// ~~~~~~~~~~~~~~~~ CART2 ~~~~~~~~~~~~~~~~~~~
+		
+		
+		System.out.println(cart2.getTotalcarts()+ " customer(s) in checking out with " + cart2.getTotalitems() + " items");
+		
+		register.scanAllItems(cart2.unloadAllItems());
+		register.printreceipt(cart2);
+		
+		
+		
+		System.out.println("Total purchase:  $" + df2.format(register.gettotalsale()));
+		//System.out.println("Total items:  " + register.getTotal());
+		
+		int totalcust=cart1.getTotalcarts()+cart2.getTotalcarts();
+/* 		System.out.println("No of customers "+ totalcust);
+ */		
+		register.printreport();
+		
+	}
+     
+	  /*
+	   ShoppingCart obj=new ShoppingCart();
+	  
+	    DataInputStream in = new DataInputStream(file2);
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        String strLine;
+
+        
+	  
+	  */
+	  
+
+	 
+
+
+	
+	
+	/* public static int getIndexbyname(String prodname, List<Product> prodlist)
+	{
+		for(Product item : prodlist)
+		{
+			if( prodname.equals(item.getName()))
+				return prodlist.indexOf(item);
+		}
+		
+		return -1;
+	}
+	 */
+	
+
+
+
+}
+
+//javac POSmain.java
+//java POSmain prices.txt cart1.txt cart2.txt
